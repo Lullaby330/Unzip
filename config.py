f42d3d12b4b8eb132899b82c4b42e20a92b0b635
Lullaby330 <69coder@gmail.com>
2023-05-07 13:20:03 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ import os
 class Config:
     APP_ID = int(os.environ.get("APP_ID", "6216349"))
     API_HASH = os.environ.get("API_HASH", "5c7418e9f3df6db931caa7354521c55f")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5615677983:AAEkoaVfsZRURf7EX_aimf8Snw2JCF0qP_U")
-    LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL", "1942188470"))
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6123506894:AAEn69pEzE7qrtYIsvmgIqOV0L7loDvLkEo")
+    LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL", "-1001971221744"))
     MONGODB_URL = os.environ.get("MONGODB_URL", "mongodb+srv://asubang:asubang@cluster0.gbyc7wo.mongodb.net/?retryWrites=true&w=majority")
-    BOT_OWNER = int(os.environ.get("BOT_OWNER", "1823080600"))
+    BOT_OWNER = int(os.environ.get("BOT_OWNER", "5280319640"))
     DOWNLOAD_LOCATION = f"{os.path.dirname(__file__)}/Downloaded"
     THUMB_LOCATION = f"{os.path.dirname(__file__)}/Thumbnails"
     TG_MAX_SIZE = 2040108421
